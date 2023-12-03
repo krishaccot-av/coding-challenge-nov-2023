557bc5ab2232dfb31d766baf060657a75e4f7924
krishaccot-av <krish.accottillam@avanade.com>
2023-12-03 00:40:22 -0800

    Key moved from env to original file

--- national-parks-ui/app/parks/api/[slug]/route.js
+++ national-parks-ui/app/parks/api/[slug]/route.js
@@ -1,5 +1,6 @@
 export async function GET(request,{params}) {
-    const res = await fetch(`https://developer.nps.gov/api/v1/parks?parkCode=${params.slug}&api_key=${process.env.DATA_API_KEY}`, {
+  const api_key = 'Kra1CV8uIShxgLqRV7e2HrGjziKFKuLr4xIyvAb1';
+    const res = await fetch(`https://developer.nps.gov/api/v1/parks?parkCode=${params.slug}&api_key=${api_key}`, {
       headers: {
         'Content-Type': 'application/json',
         'API-Key': process.env.DATA_API_KEY,
